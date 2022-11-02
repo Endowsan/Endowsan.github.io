5f23ece3704558e786b8b43d16884e97c278179a
Endowsan <72487376+Endowsan@users.noreply.github.com>
2022-11-03 03:20:08 +0900

    na

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.GITHUB_ACTIONS ? "/endow7.com": "",
+  trailingSlash: true,
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: "/"
 }
 
 module.exports = nextConfig
