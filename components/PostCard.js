aa955417d36cce1c91864045baf77b9c0b9a212a
Endowsan <72487376+Endowsan@users.noreply.github.com>
2022-12-03 23:52:29 +0900

    add categories

--- components/PostCard.js
+++ components/PostCard.js
@@ -3,14 +3,24 @@ import Link from 'next/link';
 
 const PostCard = ({post}) => {
     return (
+        <div className = 'border rounded-lg'>
         <Link href={`/posts/${post.slug}`}>
-            <div className = 'border rounded-lg'>
-            </div>
             <div className = 'px-2 py-4'>
                 <h1 className = 'font-bold text-lg'>{post.frontMatter.title}</h1>
                 <span>{post.frontMatter.date}</span>
+                <div className="prose prose-lg max-w-none">
+                    <span>category:</span>
+                    {post.frontMatter.categories.map((category) => (
+                    <span key={category}>
+                        <Link href={`/categories/${category}`}>
+                            {category}
+                        </Link>
+                    </span>
+                    ))}
+                </div>
             </div>
         </Link>
+        </div>
     );
 };
 
